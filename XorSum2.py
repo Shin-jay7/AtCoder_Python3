194dbfe0a1faa44e8bf3846372756c0cb5e877c7
Shin Ikeji <shin@jay7.net>
2019-05-21 10:11:06 +0900

    Add new file

--- XorSum2.py
+++ XorSum2.py
@@ -1,3 +1,15 @@
 # Task: https://atcoder.jp/contests/abc098/tasks/arc098_b
+_ = input()
+N = list(map(int, input().split()))
+xor_=sum_=l=ans=0
 
+for r,n in enumerate(N):
+    xor_ ^= n
+    sum_ += n
+    while xor_ < sum_:
+        xor_ ^= N[l]
+        sum_ -= N[l]
+        l += 1
+    ans += r-l+1
 
+print(ans)
